6d39fc632138349684e314cb45566d0fcf29817e
dylan <527844046@qq.com>
2018-01-31 17:59:47 +0800

    Transformers

---
+++ app/Http/Transformers/UserTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Transformers;
+
+use App\Models\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract {
+
+    public function transform(User $item) {
+        return [
+            'id' => $item->id,
+            'telphone' => $item->telphone,
+            'username' => $item->username,
+            'avatar' => $item->avatar,
+            'created_at' => (string) $item->created_at,
+            'updated_at' => (string) $item->updated_at,
+        ];
+    }
+
+}
