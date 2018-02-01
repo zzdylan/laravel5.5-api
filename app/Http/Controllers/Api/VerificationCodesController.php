edbf23af76550107d628292c8935b1806080f2c1
dylan <527844046@qq.com>
2018-02-01 11:33:21 +0800

    添加sms.debug

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,17 +10,22 @@ class VerificationCodesController extends BaseController {
     //注册验证码
     public function store(SmsRegisterRequest $request) {
         $telphone = $request->telphone;
-        $smsCode = generate_code();
-        $res = sendSmsCode($telphone, $smsCode);
-        if (!$res['status']) {
-            return $this->response->errorInternal($res['msg'] ?? '短信发送异常');
-        }
         $expiredAt = now()->addMinutes(10);
+        $response = [
+            'telphone' => $telphone,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ];
+        $smsCode = generate_code();
+        if (!config('sms.debug')) {
+            $res = sendSmsCode($telphone, $smsCode);
+            if (!$res['status']) {
+                return $this->response->errorInternal($res['msg'] ?? '短信发送异常');
+            }
+        } else {
+            $response['sms_code'] = $smsCode;
+        }
         \Cache::put('sms_code.' . $telphone, $smsCode, $expiredAt);
-        return $this->response->array([
-                    'telphone' => $telphone,
-                    'expired_at' => $expiredAt->toDateTimeString(),
-                ])->setStatusCode(201);
+        return $this->response->array($response)->setStatusCode(201);
     }
 
 }
